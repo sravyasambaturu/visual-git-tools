9271076c7207306ff05a0d4a7f270231621c76b5
Venkata Sai Sravya S. <71900458+sravyasambaturu@users.noreply.github.com>
2026-09-22 15:04:47 -0400

    Add input validation to checkout.py

--- checkout.py
+++ checkout.py
@@ -1,6 +1,9 @@
 def calculate_total(items):
     """Return the sum of all item prices."""
     total = 0
+    if items == 0:
+        total = 0
+        
     for price in items:
         total += price
     return total
